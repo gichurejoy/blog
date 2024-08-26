18c6983b00e8a0802cab40694d1db9224bcbd781
gichurejoy <gichureshiru@gmail.com>
2024-08-26 09:43:34 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*Route::get('/', function () { 
+    return view('home');
+});*/
+
+Route::view('/', 'home')->name('home');
+Route::view('contact', 'contact')->name('contact');
+Route::view('about', 'about')->name('about');
